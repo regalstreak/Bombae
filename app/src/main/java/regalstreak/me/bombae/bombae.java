42df8d3f45430629a4966dc1a60e6d8461e610e3
Neil Agarwal <regalstreak@gmail.com>
2016-08-20 20:58:14 +0530

    Bombae: Initial Project Creation

---
+++ app/src/main/java/regalstreak/me/bombae/bombae.java
@@ -0,0 +1,13 @@
+package regalstreak.me.bombae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bombae extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bombae);
+    }
+}
