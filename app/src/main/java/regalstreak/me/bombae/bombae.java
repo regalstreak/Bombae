6ac9abadf43b7d4d087b684df905b118cc44769e
Neil Agarwal <regalstreak@gmail.com>
2016-08-21 15:18:40 +0530

    Bombae: Check if the mobile number is null

--- app/src/main/java/regalstreak/me/bombae/bombae.java
+++ app/src/main/java/regalstreak/me/bombae/bombae.java
@@ -1,14 +1,20 @@
 package regalstreak.me.bombae;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class bombae extends AppCompatActivity {
 
     private EditText mobilenotxt;
     private EditText msgtxt;
     private EditText timestxt;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,23 @@ public class bombae extends AppCompatActivity {
         mobilenotxt = (EditText) findViewById(R.id.mobilenotxt);
         msgtxt = (EditText) findViewById(R.id.msgtxt);
         timestxt = (EditText) findViewById(R.id.timestxt);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cl1);
+    }
+
+    public void startBomb(View view){
+
+        String number,text,times;
+
+        number = mobilenotxt.getText().toString();
+
+        if(number != null && !number.isEmpty()){
+
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Snackbar.make(coordinatorLayout, R.string.number_error, Snackbar.LENGTH_LONG).show();
+
+        }
+
     }
 }
