5b8b9c1e0835ebedff7c3b5f5488afb73c34f17d
Neil Agarwal <regalstreak@gmail.com>
2016-08-21 15:00:49 +0530

    Bombae: Initialise the variables

--- app/src/main/java/regalstreak/me/bombae/bombae.java
+++ app/src/main/java/regalstreak/me/bombae/bombae.java
@@ -2,12 +2,21 @@ package regalstreak.me.bombae;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class bombae extends AppCompatActivity {
+
+    private EditText mobilenotxt;
+    private EditText msgtxt;
+    private EditText timestxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bombae);
+
+        mobilenotxt = (EditText) findViewById(R.id.mobilenotxt);
+        msgtxt = (EditText) findViewById(R.id.msgtxt);
+        timestxt = (EditText) findViewById(R.id.timestxt);
     }
 }
