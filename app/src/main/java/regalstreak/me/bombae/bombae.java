ef8cd932f704f2630815ed9a8ccbf7120f28b9c8
Neil Agarwal <regalstreak@gmail.com>
2016-08-21 18:06:42 +0530

    Bombae: Show a simple warning

--- app/src/main/java/regalstreak/me/bombae/bombae.java
+++ app/src/main/java/regalstreak/me/bombae/bombae.java
@@ -1,10 +1,13 @@
 package regalstreak.me.bombae;
 
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -27,10 +30,10 @@ public class bombae extends AppCompatActivity {
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cl1);
     }
 
-    public void checkEmpty(View view){
+    // Declare the strings (Used globally)
+    public String number,text,times;
 
-        // Declare the strings
-        String number,text,times;
+    public void checkEmpty(View view){
 
         // Start by getting the mobile number.
         number = mobilenotxt.getText().toString();
@@ -46,8 +49,7 @@ public class bombae extends AppCompatActivity {
 
                 if(times != null && !times.isEmpty()){
                     // Times wasn't empty. Alert the bomber.
-                    Toast.makeText(this, "More code will be placed here lol.", Toast.LENGTH_LONG).show();
-                    // TODO: Write more here
+                    showAlert(coordinatorLayout);
                 }
                 else{
                     // Times was empty, throw a snackbar.
@@ -64,4 +66,21 @@ public class bombae extends AppCompatActivity {
             Snackbar.make(coordinatorLayout, R.string.number_error, Snackbar.LENGTH_LONG).show();
         }
     }
+
+    public void showAlert(View view){
+
+        // Format the string
+        Resources res = getResources();
+        String alert1 = String.format(res.getString(R.string.alert_msg), times, number);
+        CharSequence alert = Html.fromHtml(alert1);
+
+        // Show an alert to the guy who wanna bomb the shiz.
+        AlertDialog.Builder warning = new AlertDialog.Builder(this);
+        warning.setTitle(R.string.warning)
+               .setMessage(alert)
+               .show();
+    }
+
+
+
 }
